7ed6e211cbf56c255adbb1b41010a4400fab68bf
MoYuBin <bin8727@naver.com>
2023-07-21 14:12:36 +0900

    업로드

---
+++ routes/ToDoRoute.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const { getToDo, saveTodo, updateToDo, deleteToDo } = require('../controllers/ToDoControllers');
+
+const router = Router();
+
+router.get('/', getToDo);
+router.post('/save', saveTodo);
+router.post('/update', updateToDo);
+router.post('/delete', deleteToDo);
+
+module.exports = router;
